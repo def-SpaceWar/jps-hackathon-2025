90788d78dcaac43db68c089a0fbae85f540ae418
Aryan Ahire <aryanahire9086360780@gmail.com>
2025-06-15 19:15:01 -0400

    fixed it

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
+import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno()],
-})
+    plugins: [deno()],
+    build: {
+        target: "esnext", //browsers can handle the latest ES features
+    },
+});
